893caa29136ee61dadb04445b2390bf963d0aeb1
Umit Berber <umit.berber@trendyol.com>
2021-04-25 18:10:26 +0300

    Delete unused exception

--- src/main/java/com/experiment/game/controller/exception/MoveNotEligibleException.java
+++
@@ -1,7 +0,0 @@
-package com.experiment.game.controller.exception;
-
-public class MoveNotEligibleException extends BaseException {
-    public MoveNotEligibleException(String key, String... args) {
-        super(key, args);
-    }
-}
